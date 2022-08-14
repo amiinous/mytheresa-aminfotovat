dd97bb7ce634f3a42285672f20e52809d70ced49
Amin <aminfotovat@gmail.com>
2022-08-14 21:03:01 +0430

    add try again button

--- src/Components/Common/ErrorState.tsx
+++ src/Components/Common/ErrorState.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import styled from 'styled-components/native'
 import Icon from '@/Components/Common/Icon'
+import Button from '@/Components/UIKit/Button'
 
-const ErrorState = () => {
+interface Props {
+  retry?: () => void
+}
+
+const ErrorState = ({ retry }: Props) => {
   return (
     <Container>
       <Icon name="oops" size={200} />
+      {retry ? <TryAgainButton onPress={retry} tx="tryAgain" /> : null}
     </Container>
   )
 }
@@ -14,6 +20,10 @@ const Container = styled.View`
   flex: 1;
   align-items: center;
   justify-content: center;
+`
+
+const TryAgainButton = styled(Button)`
+  margin-top: 32px;
 `
 
 export default ErrorState
